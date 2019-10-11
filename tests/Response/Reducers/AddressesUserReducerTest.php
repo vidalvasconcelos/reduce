654af554949a4a484ca353acd8b5344fb191ada2
Vidal Vasconcelos <vasconcelosvidal@gmail.com>
2019-10-11 14:44:24 -0300

    IMprove tests

--- tests/Response/Reducers/AddressesUserReducerTest.php
+++ tests/Response/Reducers/AddressesUserReducerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Response\Reducers;
 
 use App\Response\Reducers\AddressesUserReducer;
+use Faker\Provider\Uuid;
 use PHPUnit\Framework\TestCase;
 use Tests\Response\Stubs\FakeUser;
 
@@ -21,6 +22,7 @@ class AddressesUserReducerTest extends TestCase
         $reducer    = new AddressesUserReducer();
 
         $address = [
+            'id'        => Uuid::uuid(),
             'current'   => false,
             'street'    => 'rua jackson',
             'number'    => 254,
@@ -51,6 +53,7 @@ class AddressesUserReducerTest extends TestCase
         $reducer    = new AddressesUserReducer();
 
         $address_one = [
+            'id'        => Uuid::uuid(),
             'current'   => false,
             'street'    => 'rua jackson',
             'number'    => 254,
@@ -59,6 +62,7 @@ class AddressesUserReducerTest extends TestCase
         ];
 
         $address_two = [
+            'id'        => Uuid::uuid(),
             'current'   => true,
             'street'    => 'rua michel',
             'number'    => 345,
@@ -75,6 +79,53 @@ class AddressesUserReducerTest extends TestCase
         $this->assertSame(
             [$address_one, $address_two],
             $fakeUser->attributes()[$reducer::FILTER_ATTRIBUTE]
+        );
+    }
+
+    /**
+     * @test    When user has not any address and response
+     *          contains a new address.
+     *
+     * @return  void
+     */
+    public function update_an_existing_user_address_collection(): void
+    {
+        $fakeUser   = new FakeUser();
+        $reducer    = new AddressesUserReducer();
+
+        $commonUuid = Uuid::uuid();
+
+        $address_outdated = [
+            'id'        => $commonUuid,
+            'current'   => false,
+            'street'    => 'Rua Jackson',
+            'number'    => 254,
+            'zip_code'  => '23345-300',
+            'city'      => 'NJ',
+        ];
+
+        $address_updated = [
+            'id'        => $commonUuid,
+            'current'   => false,
+            'street'    => 'Rua Michel Jackson',
+            'number'    => 300,
+            'zip_code'  => '23345-300',
+            'city'      => 'NJ',
+        ];
+
+        $fakeUser->embed('addresses', [$address_outdated]);
+
+        $reducer->__invoke($fakeUser, [
+            'addresses' => [$address_updated],
+        ]);
+
+        $userAddresses = $fakeUser->attributes()[
+            $reducer::FILTER_ATTRIBUTE
+        ];
+
+        $this->assertSame(
+            [$address_updated],
+            [$userAddresses]
         );
     }
 }
