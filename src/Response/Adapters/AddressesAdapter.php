0cf420fbfa0309bab76cdcaae0ef2713a7955b34
Vidal Vasconcelos <vasconcelosvidal@gmail.com>
2019-10-14 19:27:22 -0300

    Improve addresses transformer

--- src/Response/Adapters/AddressesAdapter.php
+++ src/Response/Adapters/AddressesAdapter.php
@@ -6,6 +6,11 @@ namespace App\Response\Adapters;
 
 final class AddressesAdapter
 {
+    /**
+     * @param array $addresses
+     * @param array $current
+     * @return array
+     */
     public function __invoke(array $addresses, array $current): array
     {
         $addressId = $current['id'];
